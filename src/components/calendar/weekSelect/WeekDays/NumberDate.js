0a1a1ad016f0c041b49e9d60ce0ae2160cfe34b2
charlesscheuer <cscheuer@wustl.edu>
2019-04-12 14:33:12 -0500

    dates

--- src/components/calendar/weekSelect/WeekDays/NumberDate.js
+++ src/components/calendar/weekSelect/WeekDays/NumberDate.js
@@ -3,15 +3,7 @@ import React, { Component } from 'react'
 export default class NumberDate extends Component {
   render() {
     return (
-      <div className="numberDate">
-        <div className="numberDate_num">
-            <p className="numberDate_num-digit">{this.props.num}</p>
-        </div>
-        <div className="numberDate_th">
-        <p className="numberDate_th-letter">th</p>
-        </div>
-        
-      </div>
+        <p className="numberDate">{this.props.num}<sup>th</sup></p> 
     )
   }
 }
