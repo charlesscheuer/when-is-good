310f54e987c7327752894fd13d412320fe97cfda
kakaly <reach.kkarthik@gmail.com>
2019-05-01 07:44:22 -0700

    TimeSelect component state data

--- src/components/calendar/timeSelect/timeSelect.js
+++ src/components/calendar/timeSelect/timeSelect.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react'
 
 export default class TimeSelect extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      times: [
+        "April 4, 18 00:00",
+        "April 4, 18 00:30",
+        "April 4, 18 01:00",
+        "April 4, 18 01:30",
+        "April 4, 18 02:00",
+        "April 4, 18 02:30",
+        "April 4, 18 03:00",
+        "April 5, 18 00:00",
+        "April 5, 18 00:30",
+        "April 5, 18 01:00",
+        "April 5, 18 01:30",
+        "April 5, 18 02:00",
+        "April 5, 18 02:30",
+        "April 5, 18 03:00",
+        "April 6, 18 00:00",
+        "April 6, 18 00:30",
+        "April 6, 18 01:00",
+        "April 6, 18 01:30",
+        "April 6, 18 02:00",
+        "April 6, 18 02:30",
+        "April 6, 18 03:00",
+        "April 7, 18 00:00",
+        "April 7, 18 00:30",
+        "April 7, 18 01:00",
+        "April 7, 18 01:30",
+        "April 7, 18 02:00",
+        "April 7, 18 02:30",
+        "April 7, 18 03:00",
+        "April 8, 18 00:00",
+        "April 8, 18 00:30",
+        "April 8, 18 01:00",
+        "April 8, 18 01:30",
+        "April 8, 18 02:00",
+        "April 8, 18 02:30",
+        "April 8, 18 03:00",
+        "April 9, 18 00:00",
+        "April 9, 18 00:30",
+        "April 9, 18 01:00",
+        "April 9, 18 01:30",
+        "April 9, 18 02:00",
+        "April 9, 18 02:30",
+        "April 9, 18 03:00",
+        "April 10, 18 00:00",
+        "April 10, 18 00:30",
+        "April 10, 18 01:00",
+        "April 10, 18 01:30",
+        "April 10, 18 02:00",
+        "April 10, 18 02:30",
+        "April 10, 18 03:00",
+      ]
+    }
+  }
   render() {
-    // check the calendar.scss file for the styles here
+    let times = [...this.state.times]
     return (
       <div className="TimeSelect">
         <div className="TimeSelect_containTitle">
