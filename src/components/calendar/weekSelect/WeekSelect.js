ae4ff69a24086bce936648d2ba002c25eefca38e
kakaly <reach.kkarthik@gmail.com>
2019-05-15 18:29:29 -0700

    Refactor WeekSelect

--- src/components/calendar/weekSelect/WeekSelect.js
+++ src/components/calendar/weekSelect/WeekSelect.js
@@ -3,16 +3,7 @@ import LeftArrowButton from './LeftArrowButton'
 import RightArrowButton from './RightArrowButton'
 
 export default class WeekSelect extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      days: [],
-    }
-    this.calculateDays = this.calculateDays.bind(this)
-
-  }
-
-  calculateDays() {
+  render() {
     var map = {
       0: 'January',
       1: 'February',
@@ -31,30 +22,19 @@ export default class WeekSelect extends Component {
       var dateTimeObj = new Date(datetime)
       return [dateTimeObj.getDate(), map[dateTimeObj.getMonth()]]
     })
-    return days
-  }
-
-  componentWillMount() {
-    var days = this.calculateDays()
-    this.setState({
-      days: days
-    })
-  }
-
-  render() {
     return (
       <div className="week">
         <LeftArrowButton clicked={() => this.props.weekButtonHandler(false)} />
         <div className="week_title">
           {this.props.vw < 624 ? (
             <p className="week_title">
-              {this.state.days[0][1]}{' '}
-              {this.state.days[0][0]}
+              {days[0][1]}{' '}
+              {days[0][0]}
             </p>
           ) : (
             <p className="week_title">
-              {this.state.days[0][1]} {this.state.days[0][0]}
-              –{this.state.days[6][0]}
+              {days[0][1]} {days[0][0]}
+              –{days[6][0]}
             </p>
           )}
         </div>
