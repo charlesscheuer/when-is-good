40f574d5f85d9ccaacc3f16415a703185e35c9e5
kakaly <reach.kkarthik@gmail.com>
2019-05-16 08:05:49 -0700

    Refactor range selector

--- src/components/App.js
+++ src/components/App.js
@@ -73,112 +73,41 @@ class App extends Component {
 
   betweenTimes = value => {
     // this function determines the times to show the user as they set using the range input
-
-    // 0 should be 5 am, 50 should be 12 pm,  and 100 is 11pm
-    // START TIMES:
-    // let times = {
-    //   '0': '5 am',
-    //   '5': '6 am',
-    //   '10': '7 am',
-    //   '15': '8 am',
-    //   '20': '9 am',
-    //   '25': '10 am',
-    //   '30': '11 am',
-    //   '35': '12 pm',
-    //   '40': '1 pm',
-    //   '45': '2 pm',
-    //   '50': '3 pm',
-    //   '55': '4 pm',
-    //   '60': '5 pm',
-    //   '65': '6 pm',
-    //   '70': '7 pm',
-    //   '75': '8 pm',
-    //   '80': '9 pm',
-    //   '85': '10 pm',
-    //   '90': '11 pm'
-    // }
-    // this.setstate(times.map[lowerValue])
-    if (value[0] < 5) {
-      this.setState({ startTime: '5 am' });
-    } else if (value[0] >= 5 && value[0] < 10) {
-      this.setState({ startTime: '6 am' });
-    } else if (value[0] >= 10 && value[0] < 15) {
-      this.setState({ startTime: '7 am' });
-    } else if (value[0] >= 15 && value[0] < 20) {
-      this.setState({ startTime: '8 am' });
-    } else if (value[0] >= 20 && value[0] < 25) {
-      this.setState({ startTime: '9 am' });
-    } else if (value[0] >= 25 && value[0] < 30) {
-      this.setState({ startTime: '10 am' });
-    } else if (value[0] >= 30 && value[0] < 35) {
-      this.setState({ startTime: '11 am' });
-    } else if (value[0] >= 35 && value[0] < 40) {
-      this.setState({ startTime: '12 pm' });
-    } else if (value[0] >= 40 && value[0] < 45) {
-      this.setState({ startTime: '1 pm' });
-    } else if (value[0] >= 45 && value[0] < 50) {
-      this.setState({ startTime: '2 pm' });
-    } else if (value[0] >= 50 && value[0] < 55) {
-      this.setState({ startTime: '3 pm' });
-    } else if (value[0] >= 55 && value[0] < 60) {
-      this.setState({ startTime: '4 pm' });
-    } else if (value[0] >= 60 && value[0] < 65) {
-      this.setState({ startTime: '5 pm' });
-    } else if (value[0] >= 65 && value[0] < 70) {
-      this.setState({ startTime: '6 pm' });
-    } else if (value[0] >= 70 && value[0] < 75) {
-      this.setState({ startTime: '7 pm' });
-    } else if (value[0] >= 75 && value[0] < 80) {
-      this.setState({ startTime: '8 pm' });
-    } else if (value[0] >= 80 && value[0] < 87) {
-      this.setState({ startTime: '9 pm' });
-    } else if (value[0] >= 87 && value[0] < 95) {
-      this.setState({ startTime: '10 pm' });
-    } else if (value[0] >= 95 && value[0] < 100) {
-      this.setState({ startTime: '11 pm' });
+    let timesMap = {
+      '0-4': '5 am',
+      '5-9': '6 am',
+      '10-14': '7 am',
+      '15-19': '8 am',
+      '20-24': '9 am',
+      '25-29': '10 am',
+      '30-34': '11 am',
+      '35-39': '12 pm',
+      '40-44': '1 pm',
+      '45-49': '2 pm',
+      '50-54': '3 pm',
+      '55-59': '4 pm',
+      '60-64': '5 pm',
+      '65-69': '6 pm',
+      '70-74': '7 pm',
+      '75-79': '8 pm',
+      '80-86': '9 pm',
+      '87-94': '10 pm',
+      '95-100': '11 pm'
     }
-    if (value[1] < 5) {
-      this.setState({ endTime: '5 am' });
-    } else if (value[1] >= 5 && value[1] < 10) {
-      this.setState({ endTime: '6 am' });
-    } else if (value[1] >= 10 && value[1] < 15) {
-      this.setState({ endTime: '7 am' });
-    } else if (value[1] >= 15 && value[1] < 20) {
-      this.setState({ endTime: '8 am' });
-    } else if (value[1] >= 20 && value[1] < 25) {
-      this.setState({ endTime: '9 am' });
-    } else if (value[1] >= 25 && value[1] < 30) {
-      this.setState({ endTime: '10 am' });
-    } else if (value[1] >= 30 && value[1] < 35) {
-      this.setState({ endTime: '11 am' });
-    } else if (value[1] >= 35 && value[1] < 40) {
-      this.setState({ endTime: '12 pm' });
-    } else if (value[1] >= 40 && value[1] < 45) {
-      this.setState({ endTime: '1 pm' });
-    } else if (value[1] >= 45 && value[1] < 50) {
-      this.setState({ endTime: '2 pm' });
-    } else if (value[1] >= 50 && value[1] < 55) {
-      this.setState({ endTime: '3 pm' });
-    } else if (value[1] >= 55 && value[1] < 60) {
-      this.setState({ endTime: '4 pm' });
-    } else if (value[1] >= 60 && value[1] < 65) {
-      this.setState({ endTime: '5 pm' });
-    } else if (value[1] >= 65 && value[1] < 70) {
-      this.setState({ endTime: '6 pm' });
-    } else if (value[1] >= 70 && value[1] < 75) {
-      this.setState({ endTime: '7 pm' });
-    } else if (value[1] >= 75 && value[1] < 80) {
-      this.setState({ endTime: '8 pm' });
-    } else if (value[1] >= 80 && value[1] < 87) {
-      this.setState({ endTime: '9 pm' });
-    } else if (value[1] >= 87 && value[1] < 95) {
-      this.setState({ endTime: '10 pm' });
-    } else if (value[1] >= 95 && value[1] < 100) {
-      this.setState({ endTime: '11 pm' });
+    for(var key in timesMap) {
+      var range = key.split('-')
+      var val = timesMap[key]
+      if(value[0] >= range[0] && value[0] < range[1]) {
+        this.setState({ startTime: val})
+      }
+      if(value[1] >= range[0] && value[1] < range[1]) {
+        this.setState({ endTime: val})
+      }
     }
-  };
+  }
 
   onSliderChange = value => {
+    console.log(value)
     // this changes the state of the value array when user drags the range component from '/create'
     this.setState({
       value
