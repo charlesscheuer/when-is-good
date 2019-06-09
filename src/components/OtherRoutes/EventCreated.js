deaa3d86c1371f68132eb477f1d061f771bef8d1
charlesscheuer <cscheuer@wustl.edu>
2019-06-09 14:50:38 -0700

    added styling for other stuff

--- src/components/OtherRoutes/EventCreated.js
+++ src/components/OtherRoutes/EventCreated.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Loader from '../calendar/Loader'
-import CalendarIcon from '../calendar/CalendarIcon'
+import React from 'react';
+import Loader from '../calendar/Loader';
+import CalendarIcon from '../calendar/CalendarIcon';
 import '../../SASS/main.scss';
 
-var EventCreated = (props) => { 
-  return props.eventCode.length? (
+var EventCreated = props => {
+  return props.eventCode.length ? (
     <div>
       <div className="bar">
         <div className="brand">
@@ -14,12 +14,17 @@ var EventCreated = (props) => {
           </div>
         </div>
       </div>
-      <div className="create">
-        <p>You can share this URL with your invitees</p>
-        <a href={`http://localhost:3000/event/${props.eventCode}`}>{`https://imfree.fyi/event/${props.eventCode}`}</a>
+      <div className="sharing">
+        <p>You can share this URL with your invitees:</p>
+        <a
+          className="sharing_link"
+          href={`http://localhost:3000/event/${props.eventCode}`}
+        >{`https://imfree.fyi/event/${props.eventCode}`}</a>
       </div>
     </div>
-  ) : <Loader />
-}
+  ) : (
+    <Loader />
+  );
+};
 
-export default EventCreated
+export default EventCreated;
--- src/components/topBar/InviteeTopBar.js
+++ src/components/topBar/InviteeTopBar.js
@@ -1,59 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-const InviteeTopBar = (props) => {
+const InviteeTopBar = props => {
   return (
     <div className="topbar">
       <div className="container">
         <div className="create">
           <p>Confirm your availability</p>
-        <div className="invitee_details">
-          <form className="create_emails_form">
-            <input
-              className="create_emails_form_input"
-              placeholder="Name"
-              id="inviteeName"
-              onChange={(e) => props.inviteeDetailsHandler(e)}
-              type="text"
-            />
-            <label
-              className="create_emails_form_input_label"
-            >
-              Name
-            </label>
-          </form>
-          <form className="create_emails_form">
-            <input
-              className="create_emails_form_input"
-              placeholder="Email"
-              id="inviteeEmail"
-              onChange={(e) => props.inviteeDetailsHandler(e)}
-              type="text"
-            />
-            <label
-              className="create_emails_form_input_label"
-            >
-              Email
-            </label>
-          </form>
-          <form className="create_emails_form">
-            <input
-              className="create_emails_form_input"
-              placeholder="Phone number"
-              id="inviteeNumber"
-              onChange={(e) => props.inviteeDetailsHandler(e)}
-              type="text"
-            />
-            <label
-              className="create_emails_form_input_label"
-            >
-              Phone number
-            </label>
-          </form>
+          <div className="invitee_details">
+            <form className="create_emails_form invitee_details_form">
+              <input
+                className="create_emails_form_input"
+                placeholder="Name"
+                id="inviteeName"
+                onChange={e => props.inviteeDetailsHandler(e)}
+                type="text"
+              />
+              <label className="create_emails_form_input_label">Name</label>
+            </form>
+            <form className="create_emails_form invitee_details_form">
+              <input
+                className="create_emails_form_input"
+                placeholder="Email"
+                id="inviteeEmail"
+                onChange={e => props.inviteeDetailsHandler(e)}
+                type="text"
+              />
+              <label className="create_emails_form_input_label">Email</label>
+            </form>
+            <form className="create_emails_form invitee_details_form">
+              <input
+                className="create_emails_form_input"
+                placeholder="Phone number"
+                id="inviteeNumber"
+                onChange={e => props.inviteeDetailsHandler(e)}
+                type="text"
+              />
+              <label className="create_emails_form_input_label">
+                Phone number
+              </label>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InviteeTopBar
+export default InviteeTopBar;
