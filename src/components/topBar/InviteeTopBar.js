22239f8a76c9ebcb407ca7e6ba1ddaec25e79af0
charlesscheuer <cscheuer@wustl.edu>
2019-08-12 19:21:39 -0400

    fixed text

--- src/components/topBar/InviteeTopBar.js
+++ src/components/topBar/InviteeTopBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const InviteeTopBar = props => {
   return (
     <div className="topbar">
       <div className="container">
         <div className="create">
-          <p>Confirm your availability</p>
+          <p>Please select a time</p>
+          <p>Available times are shown in green</p>
           <div className="create_timezone">
             <p className="create_timezone_label">
               What time zone would you like the times to be in?
@@ -36,7 +37,7 @@ const InviteeTopBar = props => {
                 className="create_emails_form_input"
                 placeholder="Name"
                 id="inviteeName"
-                onChange={e => props.inviteeDetailsHandler(e, "Email")}
+                onChange={e => props.inviteeDetailsHandler(e, 'Email')}
                 type="text"
               />
               <label className="create_emails_form_input_label">Name</label>
@@ -47,22 +48,24 @@ const InviteeTopBar = props => {
                 placeholder="Email"
                 id="inviteeEmail"
                 onChange={e => {
-                  if(emailRegex.test(String(e.target.value).toLowerCase())) {
-                    props.inviteeDetailsHandler(e, "Email")
+                  if (emailRegex.test(String(e.target.value).toLowerCase())) {
+                    props.inviteeDetailsHandler(e, 'Email');
                   } else {
-                    props.inviteeDetailsHandler(e, "Invalid Email")
+                    props.inviteeDetailsHandler(e, 'Invalid Email');
                   }
                 }}
                 type="text"
               />
-              <label className="create_emails_form_input_label">{props.emailLabel}</label>
+              <label className="create_emails_form_input_label">
+                {props.emailLabel}
+              </label>
             </form>
             <form className="create_emails_form invitee_details_form">
               <input
                 className="create_emails_form_input"
                 placeholder="Phone number"
                 id="inviteeNumber"
-                onChange={e => props.inviteeDetailsHandler(e, "Email")}
+                onChange={e => props.inviteeDetailsHandler(e, 'Email')}
                 type="text"
               />
               <label className="create_emails_form_input_label">
@@ -73,7 +76,7 @@ const InviteeTopBar = props => {
         </div>
       </div>
     </div>
-    )
-  }
+  );
+};
 
-export default InviteeTopBar
+export default InviteeTopBar;
